c5c9cc39c58c879337f31b6c418ca8fd4338faca
trinuro <66290935+trinuro@users.noreply.github.com>
2024-04-11 12:46:35 +0800

    Initialise project files with Spring Initializr

---
+++ src/test/java/com/example/egringotts/EGringottsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.egringotts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EGringottsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
